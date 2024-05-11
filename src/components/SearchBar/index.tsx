5a2181188f592fed16804b1e8efef8408a281720
kubraaygun <kubraaygun@outlook.com.tr>
2024-05-12 01:12:09 +0300

    Searchbar olusturuldu

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,31 @@
+import ReactSelect from "react-select"
+import { makes } from "../../constants"
 
 const SearchBar = () => {
+
+  /*
+  Markalar dizisini select kutuphanesinin istedigi formata cevirmemiz gerekiyor
+  'bmw' > {value:"bmw", label:"bmw"}
+  */
+
+  const options = makes.map((make) => ({
+    label: make,
+    value: make,
+  }))
+
   return (
-    <div>SearchBar</div>
+    <form className="searchbar gap-3">
+      <div className="searchbar__item">
+        <ReactSelect className="w-full text-black" options={options} />
+      </div>
+
+      <div className="searchbar__item">
+        <img width={25} src="/model-icon.png" alt="araç" className="absolute ml-4"/>
+
+        <input placeholder="örn:Civic" type="text" className="searchbar__input rounded text-black" />
+
+      </div>
+    </form>
   )
 }
 
